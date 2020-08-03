9ec80d4db3d39f0f8399380f38521b67f3b779b9
Benjamin Horsleben <benjamin@fizkerinc.dk>
2020-08-04 00:01:44 +0200

    Updated tapping on codes to give feedback that the code was tapped

---
+++ Shared/Color+SystemColors.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import UIKit
+
+extension Color {
+	static var background: Color {
+		Color(UIColor.systemBackground)
+	}
+	static var text: Color {
+		Color(UIColor.label)
+	}
+}
--- Shared/Views/KeycardDetailsView.swift
+++ Shared/Views/KeycardDetailsView.swift
@@ -8,6 +8,45 @@ struct KVPair<K: Hashable, V>: Identifiable {
 	var id: K { key }
 }
 
+func withAnimation(
+	_ animation: @escaping (Double) -> Animation,
+	delay: TimeInterval = 0.2,
+	body: @escaping () -> (),
+	afterAnimation body2: @escaping () -> ()
+) {
+	withAnimation(animation(delay), body)
+	Timer.scheduledTimer(withTimeInterval: delay, repeats: false) { _ in
+		body2()
+	}
+}
+
+struct KeycardValueView: View {
+	var key: String
+	var value: String
+	var onCopy: () -> ()
+
+	@State var isClicked = false
+
+	var body: some View {
+		HStack {
+			Text(key)
+			Text(value)
+		}
+			.padding([.top, .bottom], 8)
+			.font(Font.body.monospacedDigit())
+			.lineLimit(1)
+			.onTapGesture {
+				onCopy()
+				withAnimation(Animation.easeInOut) {
+					isClicked = true
+				} afterAnimation: {
+					isClicked = false
+				}
+			}
+			.background(isClicked ? Color.background : Color.clear)
+	}
+}
+
 struct KeycardDetailsView: View {
 	let keycard: Keycard
 	let items: [KVPair<String, String>]
@@ -22,20 +61,19 @@ struct KeycardDetailsView: View {
 	var body: some View {
 		VStack {
 			TextField("Search", text: $searchValue)
+				.keyboardType(.asciiCapableNumberPad)
 				.unredacted()
 			ScrollView {
-				LazyVGrid(columns: [GridItem(.adaptive(minimum: 120, maximum: 200))]) {
+				LazyVGrid(
+					columns: [
+						GridItem(.adaptive(minimum: 120, maximum: 200)),
+					],
+					alignment: .leading
+				) {
 					ForEach(searchValue.isEmpty ? items : items.filter { $0.key.contains(searchValue) }) { pair in
-						HStack {
-							Text(pair.key)
-							Text(pair.value)
+						KeycardValueView(key: pair.key, value: pair.value) {
+							UIPasteboard.general.string = pair.value
 						}
-							.padding([.top, .bottom], 8)
-							.font(Font.body.monospacedDigit())
-							.lineLimit(1)
-							.onTapGesture(perform: {
-								UIPasteboard.general.string = pair.value
-							})
 					}
 				}
 			}
