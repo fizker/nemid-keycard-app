db61d9f9aaa7928d969bcb8dc0f89ca37851adb8
Benjamin Horsleben <benjamin@fizkerinc.dk>
2020-09-11 00:07:01 +0200

    Added basic edit functionality

--- Shared/Views/IdentityDetailsView.swift
+++ Shared/Views/IdentityDetailsView.swift
@@ -3,6 +3,11 @@ import NemIDKeycard
 
 struct IdentityDetailsView: View {
 	@Binding var identity: Identity
+	@Environment(\.editMode) var editMode
+
+	var isEditing: Bool {
+		editMode?.wrappedValue.isEditing ?? false
+	}
 
 	var body: some View {
 		VStack(alignment: .leading) {
@@ -13,14 +18,38 @@ struct IdentityDetailsView: View {
 			}
 			.padding([.leading, .trailing])
 
-			List(identity.keycards) { keycard in
-				NavigationLink(destination: KeycardDetailsView(keycard: keycard)) {
-					Text(keycard.id)
+			List {
+				ForEach(identity.keycards) { keycard in
+					NavigationLink(destination: KeycardDetailsView(keycard: keycard)) {
+						Text(keycard.id)
+					}
+				}
+				.onDelete { indexSet in
+					identity.keycards.remove(atOffsets: indexSet)
+				}
+
+				if isEditing {
+					Button {
+						print("Add-button action should not be called because buttons are disabled in edit-mode")
+					} label: {
+						HStack {
+							Image(systemName: "plus.circle.fill")
+								.foregroundColor(.green)
+								.font(.title2)
+							Text("Add new card from clipboard")
+						}
+					}
+					.highPriorityGesture(TapGesture().onEnded {
+						let keycard = Keycard(id: "foo", keys: [:])
+						identity.keycards.append(keycard)
+					})
+					.environment(\.editMode, .constant(.inactive))
 				}
 			}
 			.listStyle(PlainListStyle())
 		}
 		.navigationTitle(identity.name)
+		.navigationBarItems(trailing: EditButton())
 	}
 }
 
@@ -29,6 +58,8 @@ struct IdentityDetailsView_Previews: PreviewProvider {
 	static var previews: some View {
 		Group {
 			IdentityDetailsView(identity: .constant(exampleIdentities[0]))
+			IdentityDetailsView(identity: .constant(exampleIdentities[0]))
+				.environment(\.editMode, .constant(.active))
 
 			NavigationView {
 				IdentityDetailsView(identity: .constant(exampleIdentities[0]))
