e0daf0cff2bcc695169a54db088912a73f40fe8a
Benjamin Horsleben <benjamin@fizkerinc.dk>
2021-06-09 10:57:48 +0200

    Wrapped IdentityList in sections and also listed all keycards

--- Shared/Views/IdentityListView.swift
+++ Shared/Views/IdentityListView.swift
@@ -5,18 +5,37 @@ struct IdentityListView: View {
 	@Binding var identities: [Identity]
 
 	var body: some View {
+		let keycards = identities
+			.flatMap { ident in ident.keycards.map { (identity: ident, keycard: $0) } }
+			.sorted { $0.keycard.id < $1.keycard.id }
+
 		List {
-			ForEach(identities.indices) { index in
-				NavigationLink(destination: IdentityDetailsView(identity: $identities[index])) {
-					VStack(alignment: .leading) {
-						Text(identities[index].name)
-						Text(formatCPR(identities[index].cpr))
+			Section(header: Text("Identities")) {
+				ForEach(identities.indices) { index in
+					NavigationLink(destination: IdentityDetailsView(identity: $identities[index])) {
+						VStack(alignment: .leading) {
+							Text(identities[index].name)
+							Text(formatCPR(identities[index].cpr))
+								.font(.caption)
+						}
+					}
+				}
+			}
+			Section(header: Text("Keycards")) {
+				ForEach(keycards.indices) { index in
+					NavigationLink(destination: KeycardDetailsView(keycard: keycards[index].keycard)) {
+						VStack(alignment: .leading) {
+							Text(formatKeycardID(keycards[index].keycard.id))
+							Text(keycards[index].identity.name)
 							.font(.caption)
+							Text(formatCPR(keycards[index].identity.cpr))
+							.font(.caption)
+						}
 					}
 				}
 			}
 		}
-		.navigationTitle("Identities")
+		.navigationTitle("NemID")
 		.editButton()
 	}
 }
